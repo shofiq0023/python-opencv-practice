65ba143dd7e8572a61cdfa0b075b3a8994254a12
shofiq0023 <shofiqulislam0023@gmail.com>
2022-05-10 16:24:22 +0600

    first commit

---
+++ read.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+
+################### Reading image ###################
+# img = cv.imread("images/Cat.jpg")
+
+# cv.imshow("Cat", img)
+
+# cv.waitKey(0)
+
+################### Reading Video files ###################
+capture = cv.VideoCapture('videos/dog.mp4');
+
+# To capture with web cam. 0, 1, 2, 3 indicates the camera connected to your device
+capture = cv.VideoCapture(0);
+
+while True:
+	isTrue, frame = capture.read()
+	cv.imshow("video", frame)
+
+	if cv.waitKey(20) & 0xFF==ord("d"):
+		break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+################### Resizing images and video ###################
+# img = cv.imread("images/boy.jpg")
+
+# cv.imshow("Boy", img)
+
+# cv.waitKey(0)
